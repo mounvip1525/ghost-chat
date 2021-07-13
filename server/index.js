bf4f48246b9b1a2210f0b9e99c8ec0aceed1c35d
Mounvi <mounvip1525@gmail.com>
2021-07-13 11:53:01 +0530

    home page complete and navigation to chat room complete

--- server/index.js
+++ server/index.js
@@ -3,21 +3,25 @@ const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
 
+const router = require('./routes');
+
 const app = express()
 const server = http.createServer(app);
 app.use(cors()); //for intercommunication between urls
 
-const port = 3001 || process.env.PORT;
-
-app.get("/",(req,res)=>{
-    res.send("OK");
-})
+const port = process.env.PORT || 3001;
 
 const io = socketIO(server);
 
-io.on("connection",()=>{
+io.on("connection",(socket)=>{
     console.log("New Connection");
+    socket.on("disconnect",()=>{
+        console.log("User disconnected");
+    })
 })
+
+app.use(router);
+
 
 server.listen(port,()=>{
     console.log(`server is running on port ${port}`)
